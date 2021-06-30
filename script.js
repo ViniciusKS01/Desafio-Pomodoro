23a38efe23fd39adb254f3092b35b62770fe8199
ViniciusKS01 <martinsv016@gmail.com>
2021-06-30 12:01:44 -0400

    Modificação script.js
    Adicionando uma função de tempo, a primeira condição é do tempo do pomodoro e a segunda é do descanso, já a terceira é para voltar os valores quando as condições anteriores terminarem.

--- script.js
+++ script.js
@@ -33,6 +33,31 @@ stop.addEventListener('click', function () {
     startTimer = undefined;
 })
 
+function timer() {
+    if (ws.innerText != 0) {
+        ws.innerText--;
+    } else if (wm.innerText != 0 && ws.innerText == 0) {
+        ws.innerText = 59;
+        wm.innerText--;
+    }
+
+    if (wm.innerText == 0 && ws.innerText == 0) {
+        if (ws.innerText != 0) {
+            ws.innerText--;
+        } else if (wm.innerText != 0 && ws.innerText == 0) {
+            ws.innerText = 59;
+            wm.innerText--;
+        }
+    }
+
+    if (wm.innerText == 0 && ws.innerText == 0 && bm.innerText == 0 && bs.innerText == 0) {
+        wm.innerText = 25;
+        ws.innerText = "00";
+        bm.innerText = 5;
+        bs.innerText = "00";
+    }
+}
+
 function stopInterval() {
     clearInterval(startTimer);
 }
